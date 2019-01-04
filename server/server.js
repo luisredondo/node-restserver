b1af8253742025f2783a25944c713cfa958e37d8
Luis Redondo <luisround@gmail.com>
2019-01-03 21:14:02 -0600

    Primer commit

---
+++ server/server.js
@@ -0,0 +1,51 @@
+require('./config/config');
+
+const express = require('express');
+const app = express();
+
+const bodyParser = require('body-parser');
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json());
+
+
+app.get('/usuario', (req, res) => {
+    res.json('get usuario');
+
+});
+
+app.post('/usuario', (req, res) => {
+
+    let body = req.body;
+
+    if (body.nombre === undefined) {
+        res.status(400).json({
+            ok: false,
+            mensaje: 'El nombre es necesario'
+        });
+    } else {
+        res.json({
+            persona: body
+        });
+    }
+});
+
+app.put('/usuario/:id', (req, res) => {
+    let id = req.params.id;
+
+    res.json({
+        id
+    });
+
+});
+
+app.delete('/usuario', (req, res) => {
+    res.json('delete usuario');
+
+});
+
+app.listen(process.env.PORT, () => {
+    console.log('Escuchando puerto: ', process.env.PORT);
+});
