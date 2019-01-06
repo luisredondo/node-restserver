1ca7f282dd5b967520181f0fecfa0158087e858e
Luis Redondo <luisround@gmail.com>
2019-01-05 23:19:23 -0600

    Variable de entorno Mondo

--- server/config/config.js
+++ server/config/config.js
@@ -17,7 +17,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe'
 } else {
-    urlDB = 'mongodb://cafe-user:abc123@ds249824.mlab.com:49824/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.urlDB = urlDB;
