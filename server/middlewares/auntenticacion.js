9647631c27fa1ab4bead2a113bc85762c8e999f2
Luis Redondo <luisround@gmail.com>
2019-01-13 23:36:18 -0600

    Subida de archivos

--- server/middlewares/auntenticacion.js
+++ server/middlewares/auntenticacion.js
@@ -42,11 +42,34 @@ let verificaAdminRole = (req, res, next) => {
             }
         })
     }
+}
 
+// =========
+// Verificar Token para Imagen
+// =========
+let verificaTokenImg = (req, res, next) => {
+    let token = req.query.token;
+
+    jwt.verify(token, process.env.SEED, (err, decoded) => {
+        if (err) {
+            return res.status(401).json({
+                ok: false,
+                err: {
+                    message: 'Token no válido'
+                }
+            })
+        }
+
+        req.usuario = decoded.usuario;
+
+        next();
+
+    });
 
 }
 
 module.exports = {
     verificaToken,
-    verificaAdminRole
+    verificaAdminRole,
+    verificaTokenImg
 }
